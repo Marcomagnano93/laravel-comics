5a30a8d518456b281c2e62d6b2eb50cbff188dc4
Marcomagnano93 <marcog.magnano@gmail.com>
2024-05-21 14:18:28 +0200

    FIRST COMMIT: add layour skeleton, laravel

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <ul>
+                <li>a</li>
+                <li>b</li>
+                <li>c</li>
+                <li>d</li>
+                <li>e</li>
+            </ul>
+        </div>
+        <div class="col-6 text-center">
+            <button type="button" class="btn btn-info">Torna alla home</button>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+            <h1>LOGO</h1>
+        </div>
+        <div class="col-4">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-4 text-center">
+            <button type="button" class="btn btn-info">Abbonati</button>
+        </div>
+    </div>
+</div>
