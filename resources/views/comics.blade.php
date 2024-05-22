3fcbba193c7b84e7444902d24c9dfeb4ed4532b2
Marcomagnano93 <marcog.magnano@gmail.com>
2024-05-22 16:31:40 +0200

    ADD: comics foreach loop

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -6,5 +6,22 @@
 <div class="container">
     <h1>I nostri Comics</h1>
 </div>
+<div class="container">
+    <div class="row">
+        @foreach ($comics as $comic)
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    @foreach ($comic as $content)
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <!-- <h5 class="card-title"></h5> -->
+                        <p class="card-text">{{ $content }}</p>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
